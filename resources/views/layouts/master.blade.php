3bde2f7571fda3093f8d0ad27a563176f2027805
林江閤 <tkmk7q8qq@gmail.com>
2020-10-31 21:16:01 +0800

    修改index超連結&修復master

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -11,15 +11,15 @@
   <title>@yield('title')</title>
 
   <!-- Bootstrap core CSS -->
-  <link href=""{{asset(asset('css/bootstrap.min.css'))}}"" rel="stylesheet">
+  <link href="{{asset('css/bootstrap.min.css')}}" rel="stylesheet">
 
   <!-- Custom fonts for this template -->
-  <link href={{asset('css/all.min.css')}} rel="stylesheet" type="text/css">
+  <link href="{{asset('css/all.min.css')}} "rel="stylesheet" type="text/css">
   <link href='https://fonts.googleapis.com/css?family=Lora:400,700,400italic,700italic' rel='stylesheet' type='text/css'>
   <link href='https://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800' rel='stylesheet' type='text/css'>
 
   <!-- Custom styles for this template -->
-  <link href={{asset('css/clean-blog.min.css')}} rel="stylesheet">
+  <link href="{{asset('css/clean-blog.min.css')}}" rel="stylesheet">
 
 </head>
 
@@ -39,14 +39,14 @@
   @include('layouts.partials.footer')
 
   <!-- Bootstrap core JavaScript -->
-  <script src={{asset('js/jquery/jquery.min.js')}}></script>
-  <script src={{asset('js/bootstrap.bundle.min.js')}}></script>
+  <script src="{{asset('js/jquery/jquery.min.js')}}"></script>
+  <script src="{{asset('js/bootstrap.bundle.min.js')}}"></script>
 
   <!-- Custom scripts for this template -->
-  <script src={{asset('js/clean-blog.min.js')}}></script>
+  <script src="{{asset('js/clean-blog.min.js')}}"></script>
   <!-- Contact Form JavaScript -->
-  <script src={{asset('js/jqBootstrapValidation.js')}}></script>
-  <script src={{asset('js/contact_me.js')}}></script>
+  <script src="{{asset('js/jqBootstrapValidation.js')}}"></script>
+  <script src="{{asset('js/contact_me.js')}}"></script>
 
 
 </body>
