b83f96704134ce5a61d1653e442b7302d2a0bc8e
林江閤 <tkmk7q8qq@gmail.com>
2020-11-07 20:30:43 +0800

    運用 change(), after()等指令來調整資料表結構

--- database/migrations/2020_11_07_122343_add_paid_to_users_table.php
+++ database/migrations/2020_11_07_122343_add_paid_to_users_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateCommentsTable extends Migration
+class AddPaidToUsersTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,11 +13,8 @@ class CreateCommentsTable extends Migration
      */
     public function up()
     {
-        Schema::create('comments', function (Blueprint $table) {
-            $table->id();
-            $table->text('content');
-            $table->bigInteger('post_id');
-            $table->timestamps();
+        Schema::table('users', function (Blueprint $table) {
+            $table->integer('paid');
         });
     }
 
@@ -28,6 +25,8 @@ class CreateCommentsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('comments');
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn('paid');
+        });
     }
 }
